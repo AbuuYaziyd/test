3a352d9210a4269e46af8ece163c400d1de490a1
Abou Yaziyd <gakibrush@gmail.com>
2022-11-25 03:54:39 +0300

    admin

--- hidaya.rf.gd/htdocs/app/Views/admin/info.php
+++ hidaya.rf.gd/htdocs/app/Views/admin/info.php
@@ -110,6 +110,28 @@
         </a>
     </div>
     <div class="col-xl-3 col-lg-6 col-12">
+        <a href="<?= base_url('admin/tasrih') ?>">
+            <div class="card pull-up">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="info"><?= lang('app.tasrihs')  ?></h3>
+                                <h6><?= lang('app.tasrihs') ?></h6>
+                            </div>
+                            <div>
+                                <i class="icon-heart info font-large-2 float-right"></i>
+                            </div>
+                        </div>
+                        <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                            <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-12">
         <a href="<?= base_url('tanfidh') ?>">
             <div class="card pull-up">
                 <div class="card-content">
@@ -132,21 +154,21 @@
         </a>
     </div>
     <div class="col-xl-3 col-lg-6 col-12">
-        <a href="<?= base_url('admin/tasrih') ?>">
+        <a href="<?= base_url('admin/tanfidh') ?>">
             <div class="card pull-up">
                 <div class="card-content">
                     <div class="card-body">
                         <div class="media d-flex">
                             <div class="media-body text-left">
-                                <h3 class="info"><?= lang('app.tasrihs')  ?></h3>
-                                <h6><?= lang('app.tasrihs') ?></h6>
+                                <h3 class="secondary"><?= lang('app.hadiya') ?></h3>
+                                <h6><?= lang('app.allHadiya') ?></h6>
                             </div>
                             <div>
-                                <i class="icon-heart info font-large-2 float-right"></i>
+                                <i class="icon-user-follow secondary font-large-2 float-right"></i>
                             </div>
                         </div>
                         <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                            <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                            <div class="progress-bar bg-gradient-x-secondary" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                     </div>
                 </div>
